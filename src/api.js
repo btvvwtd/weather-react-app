96f9ce1d343ebcdc5b227f48187bf27ac9f9bf26
Artur <arturdmytryshyn@gmail.com>
2024-11-21 16:51:05 +0100

    Resolve problem with gitignore

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': '3bd7a366b2msh6f26cd8b973dd9cp143f10jsn1cd5de346849',
+    'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '6a121bfae0d0a51a970031b484dfe974';
